1d8522529db9a07d1e9cd9f5e0106f53ed5e3a5b
Stephen Kline <sakline@stanford.edu>
2016-09-13 16:59:17 -0700

    debugging generic message.

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def webhook():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 if messaging_event.get("message"):  # someone sent us a message
+                    log(messaging_event)
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
